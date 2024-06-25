70e78f4e9cb252eab692ec1ad96de42484386cec
Alan Jui <alanjui.1960@gmail.com>
2024-06-25 10:31:28 +0800

    發行作業前置測試。

--- rime.lua
+++ rime.lua
@@ -1,24 +1,26 @@
-function get_date(input, seg, env)
-	--- 以 show_date 爲開關名或 key_binder 中 toggle 的對象
-	on = env.engine.context:get_option("show_date")
-	if on and input == "date" then
-		--- Candidate(type, start, end, text, comment)
-		yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), " 日期"))
-	end
-end
----
-function single_char_first(input, env)
-	--- 以 single_char 爲開關名或 key_binder 中 toggle 的對象
-	on = env.engine.context:get_option("single_char")
-	local cache = {}
-	for cand in input:iter() do
-		if not on or utf8.len(cand.text) == 1 then
-			yield(cand)
-		else
-			table.insert(cache, cand)
-		end
-	end
-	for i, cand in ipairs(cache) do
-		yield(cand)
-	end
-end
+require("custom")
+
+-- function get_date(input, seg, env)
+-- 	--- 以 show_date 爲開關名或 key_binder 中 toggle 的對象
+-- 	on = env.engine.context:get_option("show_date")
+-- 	if on and input == "date" then
+-- 		--- Candidate(type, start, end, text, comment)
+-- 		yield(Candidate("date", seg.start, seg._end, os.date("%Y年%m月%d日"), " 日期"))
+-- 	end
+-- end
+-- ---
+-- function single_char_first(input, env)
+-- 	--- 以 single_char 爲開關名或 key_binder 中 toggle 的對象
+-- 	on = env.engine.context:get_option("single_char")
+-- 	local cache = {}
+-- 	for cand in input:iter() do
+-- 		if not on or utf8.len(cand.text) == 1 then
+-- 			yield(cand)
+-- 		else
+-- 			table.insert(cache, cand)
+-- 		end
+-- 	end
+-- 	for i, cand in ipairs(cache) do
+-- 		yield(cand)
+-- 	end
+-- end
